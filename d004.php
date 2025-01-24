e043636f5ad30fc902d07d982f3806fd1fd9d445
Rafael Almeida <rafa261010@outlook.com>
2025-01-24 14:28:13 -0300

    arrayAssociativo

---
+++ d004.php
@@ -0,0 +1,10 @@
+<?php
+$contaBanco = [
+          "Titular" => "Rafael",
+          "Saldo" => 3000
+];
+
+echo "Informação Bancária\n";
+echo "Titular ". $contaBanco["Titular"] ."\n";
+echo "Saldo ". $contaBanco["Saldo"] ."\n";
+?>
